171ad1a181c95117736017bc368f600c839fee22
amanhaidry <amanhaidry9480@gmail.com>
2025-08-05 02:48:43 +0530

    removed env check from startserver function

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,12 +12,9 @@ const startServer = async () => {
   try {
     await connectDB();
 
-    // listen for local development
-    if (ENV.NODE_ENV !== "production") {
-      app.listen(ENV.PORT, () =>
-        console.log("Server is up and running on PORT:", ENV.PORT)
-      );
-    }
+    app.listen(ENV.PORT, () =>
+      console.log("Server is up and running on PORT:", ENV.PORT)
+    );
   } catch (error) {
     console.error("Failed to start server:", error.message);
     process.exit(1);
