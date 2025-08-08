c814fce88b6b5093d84b8ed190da21a51b41036c
amanhaidry <amanhaidry9480@gmail.com>
2025-08-08 18:19:24 +0530

    auth page and functionality implemented

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,5 @@
-// import { ClerkProvider } from "@clerk/clerk-expo";
-// import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Stack } from "expo-router";
 import "../global.css";
 // import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -9,14 +9,14 @@ import "../global.css";
 
 export default function RootLayout() {
   return (
-    // <ClerkProvider tokenCache={tokenCache}>
-      // <QueryClientProvider client={queryClient}>
+    <ClerkProvider tokenCache={tokenCache}>
+       {/* <QueryClientProvider client={queryClient}> */}
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="(auth)" />
           <Stack.Screen name="(tabs)" />
         </Stack>
-        // <StatusBar style="dark" />
-      // </QueryClientProvider>
-    // </ClerkProvider>
+        {/* <StatusBar style="dark" /> */}
+      {/* </QueryClientProvider> */}
+    </ClerkProvider>
   );
 }
