4773ea6b48f5faeab8aa67d5bea321b0ca7ca903
amanhaidry <amanhaidry9480@gmail.com>
2025-08-15 23:53:58 +0530

    notification functionality impleted

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -3,7 +3,7 @@ import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import { Stack } from "expo-router";
 import "../global.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { StatusBar } from "expo-status-bar";
+import { StatusBar } from "expo-status-bar";
 
 const queryClient = new QueryClient();
 
@@ -15,7 +15,7 @@ export default function RootLayout() {
           <Stack.Screen name="(auth)" />
           <Stack.Screen name="(tabs)" />
         </Stack>
-        {/* <StatusBar style="dark" /> */}
+        <StatusBar style="dark" />
       </QueryClientProvider>
     </ClerkProvider>
   );
